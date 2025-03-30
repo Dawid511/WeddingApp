c42d1e9475ac8bdaab018863ebe552d067bfd8db
Patrycja <pjablonska@student.agh.edu.pl>
2025-03-31 00:57:08 +0200

    Adding bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent implements OnInit {
 
   http = inject(HttpClient);
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
   users: any;
 
   ngOnInit(): void {
-    this.http.get('http://localhost:5273/api/users').subscribe({
+    this.http.get('https://localhost:5001/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log('Request has completed.')
