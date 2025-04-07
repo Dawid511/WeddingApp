9a32f6c16384b6932340d2645e6234d38cdec8e8
Dawid <da.gra1@wp.pl>
2025-04-07 12:49:36 +0200

    jwt

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,15 +7,16 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController(DataContext context) : BaseApiController
+    public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
     {
         [HttpPost("register")] // account/register
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
             using var hmac = new HMACSHA512();
@@ -29,7 +30,29 @@ namespace API.Controllers
             context.Users.Add(user);
             await context.SaveChangesAsync();
 
-            return user;
+            return new UserDto{
+                Username = user.UserName,
+                Token = tokenService.CreateToken(user)
+            };
+        }
+
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto){
+            var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+           if (user == null) return Unauthorized("Invalid username");
+
+           using var hmac = new HMACSHA512(user.PasswordSalt);
+           var ComputeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+           
+           for (int i = 0; i < ComputeHash.Length; i++){
+                if(ComputeHash[i] != user.PasswordHash[i]);
+                    return Unauthorized("Invalid Password");
+           }
+
+           return new UserDto{
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+           };
         }
 
         private async Task<bool> UserExists(string username){
