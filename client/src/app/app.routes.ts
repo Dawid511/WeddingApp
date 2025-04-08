4ad1ce54e14a3aaa4d368d207154c427906f7124
Patrycja <pjablonska@student.agh.edu.pl>
2025-04-08 15:41:41 +0200

    home, nav i register/login

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { TodoComponent } from './todo/todo.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { InspirationsComponent } from './inspirations/inspirations.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'lists', component: ListsComponent},
+    {path: 'todo', component: TodoComponent},
+    {path: 'member/:id', component: MemberDetailComponent},
+    {path: 'inspirations', component: InspirationsComponent},
+    {path: '**', component: HomeComponent, pathMatch: 'full'},
+];
