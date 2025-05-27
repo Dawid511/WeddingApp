7511fb1658c47b7dba8db638d8d69c661646347c
Patrycja <pjablonska@student.agh.edu.pl>
2025-05-27 15:44:56 +0200

    Guest list page improvement

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -10,24 +10,26 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent {
- newGuestName: string = '';
+  newGuestName: string = '';
   newGuestCount: number = 1;
 
   guestList: Guest[] = [];
 
   addGuest() {
-    const trimmedName = this.newGuestName.trim();
+    const trimmedName = this.newGuestName.trim(); //usuwanie białych znaków
     if (trimmedName && this.newGuestCount > 0) {
       this.guestList.push({
         name: trimmedName,
         invited: false,
         confirmed: false,
+        editing: false, 
         count: this.newGuestCount
       });
       this.resetForm();
     }
   }
 
+  //licznik całościowej sumy gości
   totalGuestCount(): number {
     return this.guestList.reduce((sum, guest) => sum + guest.count, 0);
   }
@@ -36,11 +38,31 @@ export class ListsComponent {
     this.newGuestName = '';
     this.newGuestCount = 1;
   }
+
+  //usuwanie gości
+  deleteGuestList(index: number) {
+    this.guestList.splice(index, 1);
+  }
+
+  //edytowanie gości
+  editGuestList(index: number) {
+    this.guestList[index].editing = true;
+  }
+
+  //zapisywanie zmian po edytowaniu
+  saveGuestList(index: number, newText: string) {
+    if (newText.trim()) {
+      this.guestList[index].name = newText.trim();
+      this.guestList[index].editing = false;
+    }
+  }
 }
+
 
 interface Guest {
   name: string;
   invited: boolean;
   confirmed: boolean;
+  editing: boolean;
   count: number;
 }
